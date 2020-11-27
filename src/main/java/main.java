d24d22270fdc5f7178272150a4a87e07a298e627
flox98 <flox98@yandex.ru>
2020-11-27 15:14:22 +0500

    initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,57 @@
+public class main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//    }
+
+//    public static void main(String[] args) {
+//        byte a = -120;
+//        short b = 12442;
+//        int c = 1000;
+//        long d = 200000L;
+//        float e = 12.23f;
+//        double f = -123.123;
+//        boolean i = false;
+//    }
+//       public static void main(String[] args) {
+//           double a = 3.0;
+//           double b = 6.0;
+//           double c = 5.0;
+//           double d = 4.0;
+//           double resultFloat = a * (b + (c / d));
+//           System.out.println(resultFloat);
+//       }
+//     public static void main(String[] args) {
+//         int a = 4;
+//         int b = 1;
+//         int c = 10;
+//         int d = 20;
+//         if ((a+b)>c && (a+b)<d){
+//             System.out.println("ok");
+//             }
+//             else {System.out.println("не ок");
+//             }
+//}
+
+//    public static void main(String[] args) {
+//        int a=-3;
+//        if (a >= 0){
+//                System.out.println(a + " положит");
+//            } else {
+//                System.out.println(a + " отриц");
+//            }
+//        }
+
+    public static boolean main (String[] args) {
+        int a=3;
+        if (a<0)
+        {return true;}
+        else {return false;}
+    }
+}
+
+
+
+
+
+
+
